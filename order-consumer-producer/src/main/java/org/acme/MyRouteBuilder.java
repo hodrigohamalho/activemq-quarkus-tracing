2bff9c1bd16de6278c08f0051472f48f9ca87223
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2023-01-10 13:26:43 -0300

    poc automation

---
+++ order-consumer-producer/src/main/java/org/acme/MyRouteBuilder.java
@@ -0,0 +1,16 @@
+package org.acme;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class MyRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("kafka:camel-book")
+            .log("Recebendo mensagem: ${body}")
+            .to("amqp:queue:book-stats");
+
+    }
+    
+}
